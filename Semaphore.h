b433d43986530ffee94c605df2fbeb97cf195d58
David Apirian <dapirian@bittorrent.com>
2015-03-06 10:14:38 -0800

    naming convention

--- Semaphore.h
+++ Semaphore.h
@@ -22,37 +22,37 @@ namespace m
         native_handle_type native_handle();
 
     private:
-        size_t                  count;
-        std::mutex              mutex;
-        std::condition_variable cv;
+        size_t                  mCount;
+        std::mutex              mMutex;
+        std::condition_variable mCv;
     };
 
     inline Semaphore::Semaphore(size_t n)
-        : count{n}
+        : mCount{n}
     {}
 
     inline void Semaphore::notify()
     {
-        std::lock_guard<std::mutex> lock{mutex};
-        ++count;
-        cv.notify_one();
+        std::lock_guard<std::mutex> lock{mMutex};
+        ++mCount;
+        mCv.notify_one();
     }
 
     inline void Semaphore::wait()
     {
-        std::unique_lock<std::mutex> lock{mutex};
-        cv.wait(lock, [&]{ return count > 0; });
-        --count;
+        std::unique_lock<std::mutex> lock{mMutex};
+        mCv.wait(lock, [&]{ return mCount > 0; });
+        --mCount;
     }
 
     template<class Rep, class Period>
     bool Semaphore::wait_for(const std::chrono::duration<Rep, Period>& d)
     {
-        std::unique_lock<std::mutex> lock{mutex};
-        auto finished = cv.wait_for(lock, d, [&]{ return count > 0; });
+        std::unique_lock<std::mutex> lock{mMutex};
+        auto finished = mCv.wait_for(lock, d, [&]{ return mCount > 0; });
 
         if (finished)
-            --count;
+            --mCount;
 
         return finished;
     }
@@ -60,17 +60,17 @@ namespace m
     template<class Clock, class Duration>
     bool Semaphore::wait_until(const std::chrono::time_point<Clock, Duration>& t)
     {
-        std::unique_lock<std::mutex> lock{mutex};
-        auto finished = cv.wait_until(lock, t, [&]{ return count > 0; });
+        std::unique_lock<std::mutex> lock{mMutex};
+        auto finished = mCv.wait_until(lock, t, [&]{ return mCount > 0; });
 
         if (finished)
-            --count;
+            --mCount;
 
         return finished;
     }
 
     inline Semaphore::native_handle_type Semaphore::native_handle()
     {
-        return cv.native_handle();
+        return mCv.native_handle();
     }
 }
