cf0cb6b7ba46b291f96b0c1a5698c89fb4742d5a
David Apirian <dapirian@bittorrent.com>
2016-11-14 10:50:51 -0800

    improve comments

--- include/m/Random.h
+++ include/m/Random.h
@@ -3,22 +3,21 @@
 #include <chrono>
 
 namespace m {
-    // Produces integer values evenly distributed across a range
+    // Produces an integer value evenly distributed across a range
     template <typename T, typename UniformRandomBitGenerator>
     typename std::enable_if<std::is_integral<T>::value, T>::type
     UniformDistribution(T min, T max, UniformRandomBitGenerator&& g) {
         return std::uniform_int_distribution<T>(min, max)(g);
     }
 
-    // Produces real values evenly distributed across a range
+    // Produces a real value evenly distributed across a range
     template <typename T, typename UniformRandomBitGenerator>
     typename std::enable_if<std::is_floating_point<T>::value, T>::type
     UniformDistribution(T min, T max, UniformRandomBitGenerator&& g) {
         return std::uniform_real_distribution<T>(min, max)(g);
     }
 
-    // Produces real or integer values evenly distributed across a range. (accepts mixed arithmetic
-    // types and will return the common type).
+    // Produces an arithmetic value evenly distributed across a range of mixed type arithmetic values
     template <typename T, typename U, typename UniformRandomBitGenerator>
     typename std::enable_if<std::is_arithmetic<T>::value && std::is_arithmetic<U>::value, typename std::common_type<T, U>::type>::type
     UniformDistribution(T min, U max, UniformRandomBitGenerator&& g) {
@@ -27,7 +26,7 @@ namespace m {
                                                std::forward<UniformRandomBitGenerator>(g));
     }
 
-    // Produces chrono::durartion values evenly distributed across a range
+    // Produces a chrono::durartion value evenly distributed across a range
     template <typename Rep1, typename Period1, typename Rep2, typename Period2, typename UniformRandomBitGenerator>
     auto UniformDistribution(const std::chrono::duration<Rep1, Period1>& min,
                              const std::chrono::duration<Rep2, Period2>& max,
@@ -39,7 +38,7 @@ namespace m {
                                               std::forward<UniformRandomBitGenerator>(g))};
     }
 
-    // Produces chrono::time_point values evenly distributed across a range
+    // Produces a chrono::time_point value evenly distributed across a range
     template <typename Clock, typename Duration1, typename Duration2, typename UniformRandomBitGenerator>
     auto UniformDistribution(const std::chrono::time_point<Clock, Duration1>& min,
                              const std::chrono::time_point<Clock, Duration2>& max,
