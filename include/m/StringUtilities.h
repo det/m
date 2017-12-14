f1eb62a8a7311c43f02db067fc69c5fdd76aa799
David Apirian <dapirian@gmail.com>
2017-12-14 11:02:01 -0800

    formatting

--- include/m/StringUtilities.h
+++ include/m/StringUtilities.h
@@ -5,11 +5,10 @@
 #include <utility>
 
 namespace m {
-    inline size_t levenshteinDistance(const std::string_view &left, const std::string_view &right)
-    {
+    inline size_t levenshteinDistance(const std::string_view &left, const std::string_view &right) {
         auto column = std::vector<size_t>(right.size() + 1);
         std::iota(column.begin(), column.end(), 0); // fill column with 0..1..2..3...
-        
+
         for (size_t i = 0; i < left.size(); ++i) {
             column[0] = i + 1;
             auto diagonal = i;
@@ -18,7 +17,7 @@ namespace m {
                 std::swap(column[j + 1], diagonal);
             }
         }
-        
+
         return column.back();
     }
 }
