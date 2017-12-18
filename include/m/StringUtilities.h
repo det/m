4b36b5d76c4fcb4d5148742dbc05b800310fbb9e
David Apirian <dapirian@gmail.com>
2017-12-18 09:50:51 -0800

    whitespace

--- include/m/StringUtilities.h
+++ include/m/StringUtilities.h
@@ -9,7 +9,7 @@ namespace m {
     size_t levenshteinDistance(ForwardIt1 start1, ForwardIt1 last1, ForwardIt2 start2, ForwardIt2 last2) {
         auto column = std::vector<size_t>(std::distance(start2, last2) + 1);
         std::iota(column.begin(), column.end(), 0); // fill column with 0..1..2..3...
-        
+
         for (auto i = start1; i != last1; ++i) {
             auto colIt = column.begin();
             auto diagonal = (*colIt)++;
@@ -21,7 +21,7 @@ namespace m {
 
         return column.back();
     }
-                                          
+
     template <class T1, class T2>
     size_t levenshteinDistance(const T1& left, const T2& right) {
         using std::begin;
