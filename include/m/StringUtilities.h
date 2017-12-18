dd168cf93bc1c2e4716c38aefb592e0b9e26a9c6
David Apirian <dapirian@gmail.com>
2017-12-18 09:50:11 -0800

    make levenshteinDistance as generic as possible

--- include/m/StringUtilities.h
+++ include/m/StringUtilities.h
@@ -5,19 +5,27 @@
 #include <utility>
 
 namespace m {
-    inline size_t levenshteinDistance(const std::string_view &left, const std::string_view &right) {
-        auto column = std::vector<size_t>(right.size() + 1);
+    template <class ForwardIt1, class ForwardIt2>
+    size_t levenshteinDistance(ForwardIt1 start1, ForwardIt1 last1, ForwardIt2 start2, ForwardIt2 last2) {
+        auto column = std::vector<size_t>(std::distance(start2, last2) + 1);
         std::iota(column.begin(), column.end(), 0); // fill column with 0..1..2..3...
-
-        for (size_t i = 0; i < left.size(); ++i) {
-            column[0] = i + 1;
-            auto diagonal = i;
-            for (size_t j = 0; j < right.size(); ++j) {
-                diagonal = std::min({column[j] + 1, column[j + 1] + 1, diagonal + (left[i] != right[j])});
-                std::swap(column[j + 1], diagonal);
+        
+        for (auto i = start1; i != last1; ++i) {
+            auto colIt = column.begin();
+            auto diagonal = (*colIt)++;
+            for (auto j = start2; j != last2; ++j, ++colIt) {
+                diagonal = std::min({*colIt + 1, colIt[1] + 1, diagonal + (*i != *j)});
+                std::swap(colIt[1], diagonal);
             }
         }
 
         return column.back();
+    }
+                                          
+    template <class T1, class T2>
+    size_t levenshteinDistance(const T1& left, const T2& right) {
+        using std::begin;
+        using std::end;
+        return levenshteinDistance(begin(left), end(left), begin(right), end(right));
     }
 }
