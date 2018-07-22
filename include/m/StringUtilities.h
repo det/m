657f3a456da62d45c3c3fafaf18dafe71d64092e
David Apirian <>
2018-07-22 10:34:26 -0700

    comment cleanup

--- include/m/StringUtilities.h
+++ include/m/StringUtilities.h
@@ -8,7 +8,7 @@ namespace m {
     template <class ForwardIt1, class ForwardIt2>
     size_t levenshteinDistance(ForwardIt1 start1, ForwardIt1 last1, ForwardIt2 start2, ForwardIt2 last2) {
         auto column = std::vector<size_t>(std::distance(start2, last2) + 1);
-        std::iota(column.begin(), column.end(), 0); // fill column with 0..1..2..3...
+        std::iota(column.begin(), column.end(), 0); // fill column with [0, 1, 2, ...]
 
         for (auto i = start1; i != last1; ++i) {
             auto colIt = column.begin();
